5333f008e2ace39b8bd85c66b3e012f6502977d0
HiroForYou <csanchezs@uni.pe>
2023-02-23 18:33:14 -0500

    main updated

--- inference.py
+++ inference.py
@@ -2,7 +2,10 @@ import torch
 from ultralytics import YOLO
 from PIL import Image, ImageDraw
 from numpy import asarray
-import io
+import numpy as np
+import cv2
+
+# import io
 
 
 weight_dir = "./weights"
@@ -69,10 +72,10 @@ class ObjectDetection:
         :param frame: Frame which has been scored.
         :return: Frame with bounding boxes and labels ploted on it.
         """
-        #frame = Image.fromarray(frame)
+        # frame = Image.fromarray(frame)
         labels, cord = results
         n = len(labels)
-        #x_shape, y_shape = frame.shape[1], frame.shape[0]
+        # x_shape, y_shape = frame.shape[1], frame.shape[0]
         for i in range(n):
             row = cord[i]
             if row[4] >= 0.2:
@@ -82,51 +85,49 @@ class ObjectDetection:
                     int(row[2]),
                     int(row[3]),
                 )
-                bgr = (0, 255, 0)            #----------------------------
-                frame = Image.fromarray(frame)
+                bgr = (0, 255, 0)  # ----------------------------
+                """ frame = Image.fromarray(frame)
                 draw = ImageDraw.Draw(frame)
                 draw.rectangle([(x1, y1), (x2, y2)], outline="red")
                 buffer = io.BytesIO()
                 frame.save(buffer, format='JPEG')
                 buffer.seek(0)
-                frame = Image.open(buffer)
-                #cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-                # cv2.putText(
-                #     frame,
-                #     self.class_to_label(labels[i]),
-                #     (x1, y1),
-                #     cv2.FONT_HERSHEY_SIMPLEX,
-                #     0.9,
-                #     bgr,
-                #     2,
-                # )
+                frame = Image.open(buffer) """
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(
+                    frame,
+                    self.class_to_label(labels[i]),
+                    (x1, y1),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    bgr,
+                    2,
+                )
                 lab = self.class_to_label(labels[i])
 
-        return frame,lab 
+        return frame, lab
 
-    def __call__(self,image_file): 
+    def __call__(self, image_file):
         """
         This function is called when class is executed, it runs the loop to read the video frame by frame,
         and write the output into a new file.
         :return: void
         """
-        #frame = cv2.imread("image/image_leafblight.JPG")
-        frame = Image.open(image_file)                              ############
-        #frame = cv2.cvtColor(numpy.array(frame), cv2.COLOR_RGB2BGR) 
-        frame = asarray(frame)                                      
+        # frame = cv2.imread("image/image_leafblight.JPG")
+        frame = Image.open(image_file)  ############
+        frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
+        frame = asarray(frame)
         results = self.score_frame(frame)
-        frame,lab = self.plot_boxes(results, frame)
+        frame, lab = self.plot_boxes(results, frame)
         # print(lab)
-        #cv2.imshow("img", frame)  #comment
-        #cv2.waitKey(0)            #comment
+        # cv2.imshow("img", frame)  #comment
+        # cv2.waitKey(0)            #comment
 
         # closing all open windows
-        #cv2.destroyAllWindows()   #comment
-        return frame,lab 
-    
+        # cv2.destroyAllWindows()   #comment
+        return frame, lab
 
-# Create a new object and execute. 
+
+# Create a new object and execute.
 # detection = ObjectDetection()
 # detection()
-
-
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from inference import ObjectDetection
 import os
 import base64
 from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -20,21 +21,24 @@ def prediction():
     try:
         if request.method == "POST":
             currentfile = request.files.get("file", "")
+            # prediction of model
+        model = ObjectDetection()
+        prediction, type = model(currentfile)
+
+        buffered = BytesIO()
+        image_result = Image.fromarray(prediction)
+        image_result = image_result.resize((400, 400))
+        image_result.save(buffered, format="JPEG")
+        image_memory = base64.b64encode(buffered.getvalue())
+
+        return render_template(
+            "result.html", type=type, img_data=image_memory.decode("utf-8")
+        )
+
     except:
         errors.append("Unable to read file. Please make sure it's valid and try again.")
-    # prediction of model
-    class_name = ObjectDetection()
-    image_result, type = class_name.__call__(currentfile)
-
-    buffered = BytesIO()
-    image_result = image_result.resize((400, 400))
-    image_result.save(buffered, format="JPEG")
-    image_memory = base64.b64encode(buffered.getvalue())
-
-    return render_template(
-        "result.html", type=type, img_data=image_memory.decode("utf-8")
-    )
+        return {"response": f"Ocurriendo los siguientes errores: {errors}"}
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", debug=True, port=os.getenv("PORT", default=5000))
